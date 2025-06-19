21ba641cdbb450a2f4542151dc4f9c23fe3c7c51
Paulo Victor Silva <paulpbi16@gmail.com>
2025-06-19 17:40:08 -0300

    feat: add deleteJoke controller

--- src/controllers/jokes.controller.ts
+++ src/controllers/jokes.controller.ts
@@ -20,3 +20,17 @@ export const createJoke = async (req: Request, res: Response) => {
     res.status(404).json({ message: "Algo deu errado ao buscar as piadas!" });
   }
 };
+
+export const deleteJoke = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  try {
+    const deletedJoke = await Jokes.findByIdAndDelete(id);
+    if (!deletedJoke) {
+      res.status(404).json({ message: "Piada não encontrada!" });
+    }
+    res.status(200).json({ message: "Piada deletada com sucesso!" });
+  } catch (error) {
+    res.status(404).json({ message: "Algo deu errado ao deletar a piada!" });
+  }
+};
