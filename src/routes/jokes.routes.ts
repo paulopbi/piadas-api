4ccde1b09f5ac1a08098c28daabe48d63e8d16e4
Paulo Victor Silva <paulpbi16@gmail.com>
2025-06-19 17:40:31 -0300

    feat: add delete joke route

--- src/routes/jokes.routes.ts
+++ src/routes/jokes.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { allJokes, createJoke } from "../controllers/jokes.controller";
+import {
+  allJokes,
+  createJoke,
+  deleteJoke,
+} from "../controllers/jokes.controller";
 
 export const router = Router();
 
 router.get("/piadas", allJokes);
 router.post("/criar", createJoke);
+router.delete("/deletar/:id", deleteJoke);
