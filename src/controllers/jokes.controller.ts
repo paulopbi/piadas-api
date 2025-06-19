f0ec91334967dee03a65dd9ce6bb2ad270487abe
Paulo Victor Silva <paulpbi16@gmail.com>
2025-06-19 17:11:01 -0300

    feat: add allJokes and createJoke controllers

---
+++ src/controllers/jokes.controller.ts
@@ -0,0 +1,22 @@
+import type { Request, Response } from "express";
+import Jokes from "../models/jokes.models";
+
+export const allJokes = async (req: Request, res: Response) => {
+  try {
+    const jokes = await Jokes.find();
+    res.status(200).json(jokes);
+  } catch (error) {
+    res.status(404).json({ message: "Algo deu errado ao buscar as piadas!" });
+  }
+};
+
+export const createJoke = async (req: Request, res: Response) => {
+  try {
+    const newJoke = await Jokes.create(req.body);
+    res
+      .status(201)
+      .json({ message: "Piada criada com sucesso!", data: newJoke });
+  } catch (error) {
+    res.status(404).json({ message: "Algo deu errado ao buscar as piadas!" });
+  }
+};
