b5378a70a270dc64bce3fb3ac5f25a13c832e752
Paulo Victor Silva <paulpbi16@gmail.com>
2025-06-21 01:23:01 -0300

    refactor: create a config folder that start a database connection

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import mongoose from "mongoose";
 import express from "express";
 import cors from "cors";
 import { router as jokesRouter } from "./routes/jokes.routes";
+import { databaseConnection } from "./config/mongodb";
 
 const app = express();
-const PORT = process.env.PORT || 3002;
 
 //middlewares
 app.use(cors());
@@ -13,20 +12,10 @@ app.use(express.json());
 //routes
 app.use("/api", jokesRouter);
 
-(async () => {
-  //start a mongoDB connection
-  try {
-    await mongoose.connect(process.env.MONGODB_URI || "");
-    console.log("Conectado com sucesso ao banco de dados!");
-  } catch (error) {
-    console.error(
-      "Algo deu errado ao se conectar com o banco de dados!",
-      error
-    );
-    process.exit(1);
-  }
-})();
+//start the mongoDB connection
+databaseConnection();
 
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
