5fe99fa08e7f636bbc14a2ae8ab2ac98e0923e55
Paulo Victor Silva <paulpbi16@gmail.com>
2025-06-21 01:58:01 -0300

    feat: add search joke by id route

--- src/controllers/jokes.controller.ts
+++ src/controllers/jokes.controller.ts
@@ -1,25 +1,6 @@
 import { type Request, type Response } from "express";
 import Jokes from "../models/jokes.model";
 
-export const getAllJokes = async (req: Request, res: Response) => {
-  try {
-    const allJokes = await Jokes.find();
-
-    if (allJokes.length === 0) {
-      res.status(404).json({ message: "Nenhuma piada encontrada" });
-      return;
-    }
-
-    res.status(200).json(allJokes);
-  } catch (error) {
-    if (error instanceof Error) {
-      res.status(500).json({ message: error.message });
-      return;
-    }
-    res.status(500).json({ message: "Erro interno do servidor" });
-  }
-};
-
 export const createJoke = async (req: Request, res: Response) => {
   try {
     const newJoke = await Jokes.create(req.body);
@@ -71,6 +52,51 @@ export const updateJoke = async (req: Request, res: Response) => {
   } catch (error) {
     if (error instanceof Error) {
       res.status(500).json({ message: error.message });
+      return;
+    }
+    res.status(500).json({ message: "Erro interno do servidor" });
+  }
+};
+
+export const getAllJokes = async (req: Request, res: Response) => {
+  try {
+    const allJokes = await Jokes.find();
+
+    if (allJokes.length === 0) {
+      res.status(404).json({ message: "Nenhuma piada encontrada" });
+      return;
+    }
+
+    res.status(200).json(allJokes);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ message: error.message });
+      return;
+    }
+    res.status(500).json({ message: "Erro interno do servidor" });
+  }
+};
+
+export const getJokeById = async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (!id) {
+    res.status(404).json({ message: "ID da piada é obrigatório" });
+    return;
+  }
+
+  try {
+    const joke = await Jokes.findById(id);
+
+    if (!joke) {
+      res.status(404).json({ message: "Piada não encontrada" });
+      return;
+    }
+
+    res.status(200).json(joke);
+  } catch (error) {
+    if (error instanceof Error) {
+      res.status(500).json({ error: error.message });
       return;
     }
     res.status(500).json({ message: "Erro interno do servidor" });
--- src/routes/jokes.route.ts
+++ src/routes/jokes.route.ts
@@ -3,12 +3,14 @@ import {
   createJoke,
   deleteJoke,
   updateJoke,
+  getJokeById,
 } from "../controllers/jokes.controller";
 import { Router } from "express";
 
 export const router = Router();
 
 router.get("/piadas", getAllJokes);
+router.get("/piadas/:id", getJokeById);
 router.post("/piadas/criar", createJoke);
 router.delete("/piadas/deletar/:id", deleteJoke);
 router.put("/piadas/atualizar/:id", updateJoke);
