1709d64b9a147af03d8569801b58c2bcbd0837ea
Paulo Victor Silva <paulpbi16@gmail.com>
2025-06-22 11:53:22 -0300

    refactor: update help response structure and include ok status

--- src/controllers/help.controller.ts
+++ src/controllers/help.controller.ts
@@ -3,20 +3,25 @@ import { type Request, type Response } from "express";
 export const getHelp = async (req: Request, res: Response) => {
   try {
     res.status(200).json({
+      ok: true,
       message:
-        "Bem vindo a rota de ajuda, abaixo estão os endpoints disponíveis",
-      ajuda: {
+        "Bem vindo a API de piadas, abaixo estão os endpoints disponíveis",
+      endpoints: {
         "/api/piadas": "Irá mostrar todas as piadas (GET)",
-        "/piadas/criar": "Permite criar uma nova piada (POST)",
-        "/piadas/atualizar/:id": "Permite atualizar uma piada pelo ID (PUT)",
-        "/piadas/deletar/:id": "Permite deletar uma piada pelo ID (DELETE)",
+        "/api/piadas/criar": "Permite criar uma nova piada (POST)",
+        "/api/piadas/atualizar/:id":
+          "Permite atualizar uma piada pelo ID (PUT)",
+        "/api/piadas/deletar/:id": "Permite deletar uma piada pelo ID (DELETE)",
+      },
+      ajuda: {
+        documentação: "https://github.com/paulopbi/piadas-api",
       },
     });
   } catch (error) {
     if (error instanceof Error) {
-      res.status(500).json({ error: error.message });
+      res.status(500).json({ error: error.message, ok: false });
       return;
     }
-    res.status(500).json({ message: "Erro interno do servidor" });
+    res.status(500).json({ message: "Erro interno do servidor", ok: false });
   }
 };
