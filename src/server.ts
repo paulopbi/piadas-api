9bf08fc8ab44e61773b87cbb28acc854cad611fe
Paulo Victor Silva <paulpbi16@gmail.com>
2025-06-22 12:48:05 -0300

    fix: update default port from 3002 to 3333 in server configuration

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.get("/", getHelp);
 //start the mongoDB connection
 databaseConnection();
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
